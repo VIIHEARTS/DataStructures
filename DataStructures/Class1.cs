1e591a9b3e2b71f2ae97a33c1302730fed808a34
VIIHEARTS <viihearts@gmail.com>
2024-09-26 11:30:48 +0100

    [Unene] Added TVShows class and Movie + Shows List

---
+++ DataStructures/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DataStructures.Program;
+
+namespace DataStructures
+{
+    internal class TVShow
+    {
+        public string Title = "Untitled";
+        public int releaseYear;
+        public double Price;
+        public MovieRating Rating;
+
+        public TVShow(string title, int releaseYear, double price, MovieRating rating)
+        {
+            this.Title = title;
+            this.releaseYear = releaseYear;
+            this.Price = price;
+            this.Rating = rating;
+        }
+
+        public override string ToString()
+        {
+            return Title += " (" + releaseYear + ") Rated " + Rating + " £" + Price.ToString("0.00");
+        }
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,89 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
-            Console.ReadKey();
+            MainMenu();
 
-            float someFloat = 3.14159f;
-            Console.WriteLine(someFloat.ToString("0.000"));
-            Console.ReadKey();
+            Console.ReadLine();
         }
-    }
-}
+
+        private static void ListMovies()
+        {
+            Movie deadpool3;
+            deadpool3.Title = "Deadpool & Wolverine";
+            deadpool3.releaseYear = 2025;
+            deadpool3.Price = 12.99;
+            deadpool3.Rating = MovieRating._15;
+
+            Console.WriteLine(deadpool3.Title += " (" + deadpool3.releaseYear + ") Rated " + deadpool3.Rating);
+        }
+        private static void ListShows()
+        {
+            List<TVShow> shows = new List<TVShow>();
+            shows.Add(new TVShow("The Lord of theRings: The Rings of Power", 2022, 0, MovieRating._15));
+            shows.Add(new TVShow("The Boys", 2019, 7.99, MovieRating._18));
+            shows.Add(new TVShow("Ben and Holly's Little Kingdom", 2009, 12.45, MovieRating._U));
+
+            for (int i = 0; i < shows.Count; i++)
+            {
+                Console.WriteLine(shows[i].ToString());
+            }
+        }
+
+        private static void MainMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please choose from the following options: \n");
+                Console.WriteLine("(1) List Movies");
+                Console.WriteLine("(2) List TVShows");
+                Console.WriteLine("\n");
+                Console.WriteLine("(0) Exit Application");
+                Console.WriteLine("\n");
+
+                string userInput = Console.ReadLine();
+
+                if(int.TryParse(userInput, out int integerInput))
+                {
+                    switch (integerInput)
+                    {
+                        case 0:
+                            Enviroment.Exit(0);
+                            break;
+                        case 1:
+                            ListMovies();
+                            Console.ReadKey();
+                            break;
+                        case 2:
+                            ListShows();
+                            Console.ReadKey();
+                            break;
+                    }
+                }
+            }
+        }
+        public enum MovieRating
+        {
+            _U,
+            _12A,
+            _12,
+            _15,
+            _18
+        }
+
+        public struct Movie
+        {
+            public string Title;
+            public int releaseYear;
+            public double Price;
+            public MovieRating Rating;
+        }
+
+
+
+
+
+
+
+
+
